76b62941aca1f1bdf29648609387d0e360e197d8
Laiane <laiane.rabello@gmail.com>
2020-06-07 17:21:10 +0100

    Primeira Aplicação do Ecoleta - NLW

---
+++ public/scripts/intro.js
@@ -0,0 +1,66 @@
+//coments
+//document.write("Hello")
+
+// variaveis, tipos de dados
+// Pode USAR var ou let
+// let myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// string pode escrever entre "" '' ``(aspas)
+//"Meg Linda"
+//'Meg Maravilhosa'
+//`Meg dog`
+
+//number
+//const n1 = 1
+//const n2 = 12
+//document.write(n1 + n2)
+
+//boolean - true ou false
+//const bTrue = true
+//const bFalse = false
+//document.write (bTrue)
+
+// Objeto possuem propriedades e funcionalidade
+//const pessoa = {
+//  altura: "1,63m",
+//  idade: 24,
+//  solteiro: false,
+//  correr(){
+//     document.write("executar uma grande logica de corre")
+//  }
+//}
+
+//pessoa.correr()
+
+//ARRAy - Vetores
+//const colecaoDeBolinhas = ["blue", "green", 1, {name: "My Name"}]
+//document.write(colecaoDeBolinhas[3].name)
+
+//FUNÇOES - Funcionalidades - Atalhos - Reuso de Código
+//Registrar função
+//function sayMyName(name) {
+//  document.write(name)  
+//}
+
+//executar
+//sayMyName("Meg")
+//sayMyName("Zora")
+
+//CONDICIONAIS
+
+//const notaFinal = 7
+
+//if( notaFinal < 5 ) {
+//  document.write("Reprovado")
+//} else {
+//  document.write("Aprovado")
+//}
+
+// LOOP REPETIÇÕES
+
+//for (i = 0; i < 10; i++) {
+//  document.write(`<p>${i}</p>`)
+//}
